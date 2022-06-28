8c99f9ac7bde00ca381d063d1de55dd6f67f8cf4
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2022-06-28 15:32:07 -0300

    Add type hints and format base.py

--- mongow/base.py
+++ mongow/base.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from typing import (
     Any,
     Dict,
+    Generic,
+    Iterable,
     List,
     Optional,
     Tuple,
@@ -15,7 +17,7 @@ from pydantic import BaseModel, Field
 
 from .instance import database
 
-T = TypeVar("T", bound=BaseModel)
+T = TypeVar("T", bound="BaseMixin")
 
 
 class PyObjectId(ObjectId):
@@ -32,21 +34,21 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
-        
+
 
 class AllOptional(pydantic.main.ModelMetaclass, type):
     def __new__(cls, name, bases, namespaces, **kwargs):
-        annotations = namespaces.get('__annotations__', {})
+        annotations = namespaces.get("__annotations__", {})
         for base in bases:
             annotations.update(getattr(base, "__annotations__", {}))
         for field in annotations:
-            if not field.startswith('__'):
+            if not field.startswith("__"):
                 annotations[field] = Optional[annotations[field]]
-        namespaces['__annotations__'] = annotations
+        namespaces["__annotations__"] = annotations
         return super().__new__(cls, name, bases, namespaces, **kwargs)
 
 
-class BaseMixin(BaseModel, metaclass=AllOptional):
+class BaseMixin(BaseModel, Generic[T], metaclass=AllOptional):
     __collection__ = "base"
 
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
@@ -60,42 +62,40 @@ class BaseMixin(BaseModel, metaclass=AllOptional):
         result = await database.database[cls.__collection__].insert_one(
             data.dict(by_alias=True)
         )
-
         return result.inserted_id
 
     @classmethod
     async def read(
-            cls,
-            fields: Tuple[str] = tuple(),
-            order: Optional[Tuple[str, bool]] = None,
-            offset: int = 0,
-            limit: int = 100,
-            filters: Dict[str, Any] = None
+        cls,
+        fields: Iterable[str] = tuple(),
+        order: Optional[Tuple[str, bool]] = None,
+        offset: int = 0,
+        limit: int = 100,
+        filters: Optional[Dict[str, Any]] = None,
     ) -> List[T]:
         if filters is None:
             filters = {}
-
         cursor = database.database[cls.__collection__].find(
-            filters,
-            {key: 1 for key in fields}
+            filters, {key: 1 for key in fields}
         )
         if order is not None:
             cursor = cursor.sort(order[0], 1 if order[1] else -1)
-
-        return await cursor.skip(offset).to_list(length=offset+limit)
+        return await cursor.skip(offset).to_list(length=offset + limit)
 
     @classmethod
-    async def update(cls, filters: Dict[str, Any], data: T | dict, operator: str) -> int:
+    async def update(
+        cls, filters: Dict[str, Any], data: T | dict, operator: str
+    ) -> int:
         if isinstance(data, dict):
             dict_data = data
         else:
             dict_data = data.dict(exclude_unset=True, by_alias=True)
-            dict_data = {key: val for key, val in dict_data.items() if key not in filters}
+            dict_data = {
+                key: val for key, val in dict_data.items() if key not in filters
+            }
         result = await database.database[cls.__collection__].update_one(
-            filters,
-            {operator: dict_data}
+            filters, {operator: dict_data}
         )
-
         return result.modified_count
 
     @classmethod
@@ -103,27 +103,22 @@ class BaseMixin(BaseModel, metaclass=AllOptional):
         dict_data = data.dict(exclude_unset=True, by_alias=True)
         dict_data = {key: val for key, val in dict_data.items() if key not in filters}
         result = await database.database[cls.__collection__].update_one(
-            filters,
-            {"$set": dict_data},
-            upsert=True,
-            return_document=True
+            filters, {"$set": dict_data}, upsert=True, return_document=True
         )
         return result.modified_count
 
     @classmethod
     async def delete(cls, filters: Dict[str, Any]) -> int:
-        result = await database.database[cls.__collection__].delete_many(
-            filter=filters
-        )
+        result = await database.database[cls.__collection__].delete_many(filter=filters)
         return result.deleted_count
 
     @classmethod
-    async def create_nested(cls, parent_id: ObjectId, child_name: str, data: T) -> Optional[ObjectId]:
+    async def create_nested(
+        cls, parent_id: ObjectId, child_name: str, data: T
+    ) -> Optional[ObjectId]:
         result = await database.database[cls.__collection__].update_one(
-            {"_id": parent_id},
-            {"$addToSet": {child_name: data.dict(by_alias=True)}}
+            {"_id": parent_id}, {"$addToSet": {child_name: data.dict(by_alias=True)}}
         )
-
         if result.modified_count:
             return data.id
         return None
@@ -133,5 +128,4 @@ class BaseMixin(BaseModel, metaclass=AllOptional):
         result = await database.database[cls.__collection__].insert_many(
             [obj.dict(by_alias=True) for obj in data]
         )
-
         return result.inserted_ids
